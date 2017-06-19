0675b078d6b656136ad3588d84699404bda776eb
ProtoFox19 <proto.fox@web.de>
2017-06-19 02:15:37 +0200

    Little Changes

---
+++ Assets/Scripts/game/JoinSessionButtonPrefab.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class JoinSessionButtonPrefab : MonoBehaviour {
+
+    public Button joinSessionButton;
+    public Text sessionIDText;
+    public Text leaderText;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+    public void setUp (Text sessionID, Text leader) {
+        sessionIDText = sessionID;
+        leaderText = leader;
+    }
+	
+
+}
--- Assets/Scripts/network/JoinSession.cs
+++ Assets/Scripts/network/JoinSession.cs
@@ -5,10 +5,12 @@ using UnityEngine;
 public class JoinSession : MonoBehaviour {
 
     public GameObject logInCanvas, mainMenuCanvas, JoinSessionCanvas, backButton;
+    public GameObject JoinButton;
+    public GameObject content;
 
     void Start () {
         JoinSessionCanvas.SetActive(false);
- 
+        AddButtons();
     }
 	
 	// Update is called once per frame
@@ -20,4 +22,17 @@ public class JoinSession : MonoBehaviour {
         mainMenuCanvas.SetActive(true);
         JoinSessionCanvas.SetActive(false);
     }
+
+
+    private void AddButtons() {
+        int count = 0;
+        for (int i = 0; i < 20; i++) {
+            GameObject newButton = (GameObject)Instantiate(JoinButton); 
+            newButton.transform.SetParent(content.transform);
+            count = i;
+        }
+        RectTransform newRT = content.GetComponent<RectTransform>();
+        newRT.sizeDelta = new Vector2(0, count * 100);
+    }
+
 }
